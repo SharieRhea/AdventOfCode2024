3201bc6edcb6ba708363d060e0e1d841094b7b01
SharieRhea <sharierhea@gmail.com>
2024-12-07 10:25:00 -0500

    improve day 6

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,4 +1,6 @@
 use std::collections::HashSet;
+use std::thread;
+use std::sync::mpsc;
 use util;
 
 fn main() {
@@ -13,23 +15,24 @@ fn main() {
             }
         }
     }
-    
-    println!("Part 1: {}", part_1(grid.clone(), start));
-    println!("Part 2: {}", part_2(grid, start));
+   
+    let visited = part_1(grid.clone(), start);
+    println!("Part 1: {}", visited.len());
+    println!("Part 2: {}", part_2(grid, start, visited));
 
 }
 
-fn part_1(mut grid: Vec<Vec<char>>, start: (i32, i32)) -> usize {
+fn part_1(grid: Vec<Vec<char>>, start: (i32, i32)) -> HashSet<(usize, usize)> {
     let width = grid[0].len();
     let height = grid.len();
 
     let mut position = start;
     let directions: Vec<(i32, i32)> = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
-    let mut direction = 0; 
+    let mut direction = 0;
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
 
     while check_bounds(position, height, width) {
-        // mark the current position
-        grid[position.1 as usize][position.0  as usize] = 'X';
+        visited.insert((position.0 as usize, position.1 as usize));
         let next_position = (position.0 + directions[direction].0, position.1 + directions[direction].1);
         if check_bounds(next_position, height, width) && grid[next_position.1 as usize][next_position.0 as usize] == '#' {
             // hit an obstacle, turn
@@ -40,28 +43,59 @@ fn part_1(mut grid: Vec<Vec<char>>, start: (i32, i32)) -> usize {
         }
     }
 
-    // count the number of marked tiles
+    return visited;
+}
+
+fn part_2(grid: Vec<Vec<char>>, start: (i32, i32), visited: HashSet<(usize, usize)>) -> usize {
     let mut sum = 0;
-    for row in grid.iter() {
-        sum += row.iter().filter(|character| **character == 'X').count();
+    let thread_count = 100;
+    // the number of obstacles that each thread will tackle
+    let size = visited.len() / thread_count;
+    let mut to_check: Vec<(usize, usize)> = visited.into_iter().collect();
+
+    // set up send and receive channel for message passing
+    let (tx, rx) = mpsc::channel();
+
+    for thread_id in 0..thread_count {
+        // clone these so ownership isn't moved into the thread
+        let thread_grid = grid.clone();
+        let thread_tx = tx.clone();
+
+        // remove a segment from the main list with length size (or shorter if  this is the last
+        // thread
+        let segment: Vec<(usize, usize)> = if thread_id == thread_count - 1 {
+            to_check.clone()
+        }
+        else { 
+            to_check.drain(0..size).collect()
+        };
+
+        thread::spawn(move || {
+            thread_tx.send(check_obstacles(thread_grid, start, segment)).unwrap();
+        });
+    }
+
+    for _thread_id in 0..thread_count {
+        // recv is blocking so this will receive from any thread that has finished until they have
+        // all sent something back
+        // this means joining on join handles is unnecessary
+        sum += rx.recv().unwrap();
     }
 
     return sum;
 }
 
-fn part_2(grid: Vec<Vec<char>>, start: (i32, i32)) -> usize {
+fn check_obstacles(grid: Vec<Vec<char>>, start: (i32, i32), visited: Vec<(usize, usize)>) -> usize {
     let mut sum = 0;
 
-    // replace every possible blank space with an obstacle and check
+    // replace each tile that was visited with an obstacle to see if it creates a cycle
     // computer go brrr :)
-    for (y, row) in grid.iter().enumerate() {
-        for (x, _column) in row.iter().enumerate() {
-            if grid[y][x] == '.' {
-                let mut new_grid = grid.clone();
-                new_grid[y][x] = '#';
-                if check_cycle(new_grid, start) {
-                    sum += 1;
-                }
+    for (x, y) in visited.iter() {
+        if grid[*y][*x] == '.' {
+            let mut new_grid = grid.clone();
+            new_grid[*y][*x] = '#';
+            if check_cycle(new_grid, start) {
+                sum += 1;
             }
         }
     }
