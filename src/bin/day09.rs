3cb1b66a9851ff1a941ac9710f39b6c1b97b790a
SharieRhea <sharierhea@gmail.com>
2024-12-09 14:22:33 -0500

    improve day 9

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,92 +1,112 @@
 use std::collections::BinaryHeap;
-
 use itertools::Itertools;
+
 use util;
 
 fn main() {
-    // NOTE: this is trash code but it's 1AM so I will come back and fix it tomorrow
-
     let line = util::read_file("day09.txt");
 
-    println!("Part 1: {}", part_1(&line));
-    println!("Part 2: {}", part_2(&line));
+    let mut disk: Vec<(i64, i64)> = vec![];
+    let mut heap: BinaryHeap<(i64, i64)> = BinaryHeap::new();
+
+    // file ids increase starting from 0
+    let mut id: i32 = 0;
+    let chars = line.chars().collect_vec();
+    for index in 0..chars.len() {
+        if index % 2 == 0 {
+            disk.push((id.into(), chars[index].to_digit(10).unwrap().into()));
+            heap.push((id.into(), chars[index].to_digit(10).unwrap().into()));
+            id += 1;
+        }
+        else {
+            disk.push((-1, chars[index].to_digit(10).unwrap().into()));
+        }
+    }
+
+    println!("Part 1: {}", part_1(disk.clone(), heap.clone()));
+    println!("Part 2: {}", part_2(disk, heap));
 }
 
 
-fn part_1(line: &str) -> i64 {
-    let mut checksum: i64 = 0;
+fn part_1(mut disk: Vec<(i64, i64)>, mut heap: BinaryHeap<(i64, i64)>) -> i64 {
+    loop {
+        // no more files to move
+        if heap.peek() == None { break; }
 
-    let mut disk: Vec<i64> = vec![];
-    let mut heap: BinaryHeap<i64> = BinaryHeap::new();
-    let mut id: i32 = 0;
-    let chars = line.chars().collect_vec();
-    for index in (0..line.len()).step_by(2) {
-        for _ in 0..chars[index].to_digit(10).unwrap() {
-            disk.push(id.into());
-            heap.push(id.into());
-        }
-        if chars.get(index + 1) != None {
-            for _ in 0..chars[index + 1].to_digit(10).unwrap() {
-                disk.push(-1);
+        let (id, length) = heap.pop().unwrap();
+        if let Some(free_space_index) = disk.iter().position(|it| it.0 == -1) {
+            let free_space = disk[free_space_index].1;
+            let remaining = free_space - length;
+            if remaining == 0 { 
+                // file matching a gap exactly, just move it and delete old file from end
+                disk.splice(free_space_index..free_space_index + 1, [(id, length)]); 
+                let position = disk.iter().rposition(|it| it.0 == id).unwrap();
+                disk.remove(position);
             }
+            else if remaining > 0 { 
+                // more free space than the file size, move the file, add the remaining space,
+                // delete old file from end
+                disk.splice(free_space_index..free_space_index + 1, [(id, length), (-1, remaining)]); 
+                let position = disk.iter().rposition(|it| it.0 == id).unwrap();
+                disk.remove(position);
+            }
+            else { 
+                // not enough space for whole file, move the portion that will fit, update the file
+                // on disk and in the heap to have the new shorter length
+                disk.splice(free_space_index..free_space_index + 1, [(id, free_space)]); 
+                heap.push((id, length - free_space));
+                let position = disk.iter().rposition(|it| it.0 == id).unwrap();
+                disk[position] = (id, length - free_space);
+            }
+
+            // remove any trailing free space from the end of the disk
+            loop {
+                let index = disk.len() - 1;
+                if disk[index].0 == -1 {
+                    disk.remove(index);
+                }
+                else { break; }
+            }
+
         }
-        id += 1;
+        else { break; }
     }
 
-    for index in 0..disk.len() {
-        if disk[index] == -1 {
-            let number = match heap.pop() {
-                Some(number) => number,
-                None => break
-            };
-            disk[index] = number;
-            let position = disk.iter().rposition(|it| *it == number).unwrap();
-            disk[position] = -1;
-        }
-    }
-
-    for (index, number) in disk.iter().enumerate() {
-        if *number != -1 {
-            checksum += index as i64 * *number;
+    let mut checksum: i64 = 0;
+    let mut index: i64 = 0;
+    for segment in disk.iter() {
+        if segment.0 == -1 { index += segment.1; }
+        else {
+            for _ in 0..segment.1 {
+                checksum += segment.0 * index;
+                index += 1;
+            }
         }
     }
 
     return checksum;
 }
 
-fn part_2(line: &str) -> i64 {
-    let mut checksum: i64 = 0;
-
-    let mut disk: Vec<(i64, i64)> = vec![];
-    let mut heap: BinaryHeap<(i64, i64)> = BinaryHeap::new();
-    let mut id: i32 = 0;
-    let chars = line.chars().collect_vec();
-    for index in (0..line.len()).step_by(2) {
-        disk.push((id.into(), chars[index].to_digit(10).unwrap().into()));
-        heap.push((id.into(), chars[index].to_digit(10).unwrap().into()));
-        if chars.get(index + 1) != None {
-            disk.push((-1, chars[index + 1].to_digit(10).unwrap().into()));
-        }
-        id += 1;
-    }
-
+fn part_2(mut disk: Vec<(i64, i64)>, mut heap: BinaryHeap<(i64, i64)>) -> i64 {
     loop {
         if heap.peek() == None { break; }
 
         let (id, length) = heap.pop().unwrap();
+        // find a gap that can accomodate this file
         if let Some(index) = disk.iter().position(|it| it.0 == -1 && it.1 >= length) {
            let space = disk[index].1;
+            // move the file and update and remaining space
             disk.splice(index..index + 1, [(id, length), (-1, space - length)]);
             let position = disk.iter().rposition(|it| it.0 == id).unwrap();
+            // remove the file from the end of disk
             disk.splice(position..position + 1, [(-1, length)]);
         }
     }
 
+    let mut checksum: i64 = 0;
     let mut index: i64 = 0;
     for segment in disk.iter() {
-        if segment.0 == -1 { 
-            index += segment.1;
-        }
+        if segment.0 == -1 { index += segment.1; }
         else {
             for _ in 0..segment.1 {
                 checksum += segment.0 * index;
